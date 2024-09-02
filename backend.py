8f790a8e1a3be8c7ba88c140d4b96e357b251125
yousefsawy <99139949+yousefsawy@users.noreply.github.com>
2024-09-02 13:07:01 +0300

    Added Scanning Capabilities

--- backend.py
+++ backend.py
@@ -1,6 +1,5 @@
 import importlib
 import PyQt5.QtWidgets
-from lxml import etree
 from openpyxl.styles import PatternFill
 from PyQt5 import QtCore, QtGui, QtWidgets
 from front import Ui_MainWindow
@@ -18,23 +17,25 @@ from PyQt5.QtWidgets import QLineEdit
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5 import QtCore, QtGui, QtWidgets, QtTest
 import sys
-import os
-import re
-from pyexcel.cookbook import merge_all_to_a_book
-import glob
-from PyQt5.QtWidgets import QMessageBox, QFileDialog
-# import win32com.client as win32
-from PyQt5.QtGui import QTextCursor
-import numpy as np
-from tabulate import tabulate
-import datetime
-import winsound
-import pandas as pd
-from openpyxl import workbook, load_workbook
+# import os
+# import re
+# # from pyexcel.cookbook import merge_all_to_a_book
+# import glob
+# from PyQt5.QtWidgets import QMessageBox, QFileDialog
+# # import win32com.client as win32
+# from PyQt5.QtGui import QTextCursor
+# # import numpy as np
+# # from tabulate import tabulate
+# import datetime
+# import winsound
+# import pandas as pd
+# from openpyxl import workbook, load_workbook
+# import openpyxl
+# import traceback
+# from openpyxl.worksheet.datavalidation import DataValidation
+# import threading
 import openpyxl
-import traceback
-from openpyxl.worksheet.datavalidation import DataValidation
-import threading
+
 
 
 class BackEndClass(QtWidgets.QWidget, Ui_MainWindow):
@@ -47,12 +48,66 @@ class BackEndClass(QtWidgets.QWidget, Ui_MainWindow):
         self.audit_btn.clicked.connect(self.audit_mode)
         self.back_btn_user.clicked.connect(self.back_menu)
         self.back_btn_audit.clicked.connect(self.back_menu)
+
+
+        # Audit Mode Buttons:
+        self.insert_btn_audit.clicked.connect(self.insert_audit)
+        self.pushButton_browse_audit.clicked.connect(self.browse_audit)
+        self.Init_View()
+
+        #Init Variables:
+        self.Excel_Name = ""
+
+
+    # Function called in __init__(), Fixes the view of user
+    def Init_View(self):
+        self.insert_btn_audit.setEnabled(False)
     def user_mode(self):
         self.tabWidget.setCurrentIndex(1)
     def audit_mode(self):
         self.tabWidget.setCurrentIndex(2)
     def back_menu(self):
         self.tabWidget.setCurrentIndex(0)
+
+    def insert_audit(self):
+        AssetTag = str(self.lineEdit_audit.text())
+        print("Assert Tag: " + AssetTag)
+        try:
+            wb = openpyxl.load_workbook(self.Excel_Name)
+            sheet = wb.active
+            for i in range(1, sheet.max_row + 1):
+                if(str(sheet.cell(row=i, column=2).value) == AssetTag):
+                    sheet.cell(row=i, column=6).value = "True"
+                    wb.save(self.Excel_Name)
+                    self.lineEdit_audit.setText("")
+                    print("here")
+        except:
+            QMessageBox.about(self, "Message", "Error ")
+
+
+    #
+    def browse_audit(self):
+        Excel_File = QFileDialog.getOpenFileName(self, 'Open File', 'Select File', '(*.xlsx)')
+        Excel_File = Excel_File[0]
+        print(Excel_File)
+
+        if Excel_File.endswith(".xlsx"):
+            self.Excel_Name = Excel_File
+            self.insert_btn_audit.setEnabled(True)
+            self.lineEdit_audit.setReadOnly(False)
+        else:
+            QMessageBox.about(self, "Message", "Please select excel file type")
+            self.insert_btn_audit.setEnabled(False)
+            self.lineEdit_audit.setReadOnly(True)
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -61,3 +116,6 @@ if __name__ == "__main__":
     ui = BackEndClass()
     MainWindow.show()
     sys.exit(app.exec_())
+
+
+
