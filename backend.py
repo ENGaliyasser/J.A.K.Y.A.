2001dd5deb3fe45c2225de66fabe2145ec67c06e
Khaled Waleed <104536807+khaledwaleedsamir@users.noreply.github.com>
2024-09-03 13:38:15 +0300

    Password in audit

--- backend.py
+++ backend.py
@@ -5,8 +5,13 @@ from PyQt5.QtWidgets import QMessageBox, QFileDialog
 
 # Assuming Ui_MainWindow is the generated class from Qt Designer
 from front import Ui_MainWindow
-
-
+# Password Check
+password = "JAKYA2024" # Audit Password
+def check_password(entered_pass):
+    if entered_pass == password:
+        return True
+    else:
+        return False
 class BackEndClass(QtWidgets.QWidget, Ui_MainWindow):
 
     def __init__(self):
@@ -40,47 +45,54 @@ class BackEndClass(QtWidgets.QWidget, Ui_MainWindow):
         self.tabWidget.setCurrentIndex(0)
 
     def browse_audit(self):
-        Excel_File = QFileDialog.getOpenFileName(self, 'Open File', 'Select File', '(*.xlsx)')
-        Excel_File = Excel_File[0]
-        print(Excel_File)
-
-        if Excel_File.endswith(".xlsx"):
-            self.Excel_Name = Excel_File
-            self.insert_btn_audit.setEnabled(True)
-            self.lineEdit_audit.setReadOnly(False)
+        entered_password = self.lineEdit_password_audit.text()
+        if check_password(entered_password) == True:
+            Excel_File = QFileDialog.getOpenFileName(self, 'Open File', 'Select File', '(*.xlsx)')
+            Excel_File = Excel_File[0]
+            print(Excel_File)
+            if Excel_File.endswith(".xlsx"):
+                self.Excel_Name = Excel_File
+                self.insert_btn_audit.setEnabled(True)
+                self.lineEdit_audit.setReadOnly(False)
+            else:
+                QMessageBox.about(self, "Message", "Please select an Excel file type")
+                self.insert_btn_audit.setEnabled(False)
+                self.lineEdit_audit.setReadOnly(True)
         else:
-            QMessageBox.about(self, "Message", "Please select an Excel file type")
-            self.insert_btn_audit.setEnabled(False)
-            self.lineEdit_audit.setReadOnly(True)
+            QMessageBox.about(self, "Message", "Please Enter the correct Password")
 
     def insert_audit(self):
-        AssetTag = str(self.lineEdit_audit.text())
-        print("Asset Tag: " + AssetTag)
-        try:
-            wb = openpyxl.load_workbook(self.Excel_Name)
-            sheet = wb.active
-            row_to_update = None
+        entered_password = self.lineEdit_password_audit.text()
+        if check_password(entered_password) == True:
+            AssetTag = str(self.lineEdit_audit.text())
+            print("Asset Tag: " + AssetTag)
+            try:
+                wb = openpyxl.load_workbook(self.Excel_Name)
+                sheet = wb.active
+                row_to_update = None
 
-            for i in range(1, sheet.max_row + 1):
-                if str(sheet.cell(row=i, column=2).value) == AssetTag:
-                    # Mark this row as "True" in column 6
-                    sheet.cell(row=i, column=6).value = "True"
-                    row_to_update = i
-                    break
+                for i in range(1, sheet.max_row + 1):
+                    if str(sheet.cell(row=i, column=2).value) == AssetTag:
+                        # Mark this row as "True" in column 6
+                        sheet.cell(row=i, column=6).value = "True"
+                        row_to_update = i
+                        break
 
-            if row_to_update:
-                # Save the workbook to persist changes
-                wb.save(self.Excel_Name)
-                # Clear the input field
-                self.lineEdit_audit.setText("")
-                print(f"Row {row_to_update} updated with 'True'")
-                # Update the display label with the updated row
-                self.display_row(row_to_update)
-            else:
-                QMessageBox.about(self, "Message", "Asset Tag not found in the Excel sheet")
+                if row_to_update:
+                    # Save the workbook to persist changes
+                    wb.save(self.Excel_Name)
+                    # Clear the input field
+                    self.lineEdit_audit.setText("")
+                    print(f"Row {row_to_update} updated with 'True'")
+                    # Update the display label with the updated row
+                    self.display_row(row_to_update)
+                else:
+                    QMessageBox.about(self, "Message", "Asset Tag not found in the Excel sheet")
 
-        except Exception as e:
-            QMessageBox.about(self, "Message", "Error: " + str(e))
+            except Exception as e:
+                QMessageBox.about(self, "Message", "Error: " + str(e))
+        else:
+            QMessageBox.about(self, "Message", "Please Enter the correct Password")
 
     def display_row(self, row_number):
         try:
