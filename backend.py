43d4fa9efff53910cc85b117f9a20284df9469f3
joee <jnader732@gmail.com>
2024-09-03 11:17:52 +0300

    sdfg

--- backend.py
+++ backend.py
@@ -1,47 +1,16 @@
-import importlib
-import PyQt5.QtWidgets
-from openpyxl.styles import PatternFill
-from PyQt5 import QtCore, QtGui, QtWidgets
-from front import Ui_MainWindow
-from PyQt5 import QtCore, QtGui, QtWidgets, QtTest
-from PyQt5.QtWidgets import QMessageBox, QFileDialog
-import re
-from openpyxl.utils import get_column_letter
-import time
-import csv
-import shutil
-import datetime
-from PyQt5.QtGui import QTextCursor
-import PyQt5.QtWidgets
-from PyQt5.QtWidgets import QLineEdit
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5 import QtCore, QtGui, QtWidgets, QtTest
 import sys
-# import os
-# import re
-# # from pyexcel.cookbook import merge_all_to_a_book
-# import glob
-# from PyQt5.QtWidgets import QMessageBox, QFileDialog
-# # import win32com.client as win32
-# from PyQt5.QtGui import QTextCursor
-# # import numpy as np
-# # from tabulate import tabulate
-# import datetime
-# import winsound
-# import pandas as pd
-# from openpyxl import workbook, load_workbook
-# import openpyxl
-# import traceback
-# from openpyxl.worksheet.datavalidation import DataValidation
-# import threading
 import openpyxl
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox, QFileDialog
 
+# Assuming Ui_MainWindow is the generated class from Qt Designer
+from front import Ui_MainWindow
 
 
 class BackEndClass(QtWidgets.QWidget, Ui_MainWindow):
 
     def __init__(self):
-        QtWidgets.QWidget.__init__(self)
+        super().__init__()
         self.setupUi(MainWindow)
         self.tabWidget.tabBar().setVisible(False)
         self.user_btn.clicked.connect(self.user_mode)
@@ -49,43 +18,27 @@ class BackEndClass(QtWidgets.QWidget, Ui_MainWindow):
         self.back_btn_user.clicked.connect(self.back_menu)
         self.back_btn_audit.clicked.connect(self.back_menu)
 
-
         # Audit Mode Buttons:
         self.insert_btn_audit.clicked.connect(self.insert_audit)
         self.pushButton_browse_audit.clicked.connect(self.browse_audit)
         self.Init_View()
 
-        #Init Variables:
+        # Init Variables:
         self.Excel_Name = ""
-
 
     # Function called in __init__(), Fixes the view of user
     def Init_View(self):
         self.insert_btn_audit.setEnabled(False)
+
     def user_mode(self):
         self.tabWidget.setCurrentIndex(1)
+
     def audit_mode(self):
         self.tabWidget.setCurrentIndex(2)
+
     def back_menu(self):
         self.tabWidget.setCurrentIndex(0)
 
-    def insert_audit(self):
-        AssetTag = str(self.lineEdit_audit.text())
-        print("Assert Tag: " + AssetTag)
-        try:
-            wb = openpyxl.load_workbook(self.Excel_Name)
-            sheet = wb.active
-            for i in range(1, sheet.max_row + 1):
-                if(str(sheet.cell(row=i, column=2).value) == AssetTag):
-                    sheet.cell(row=i, column=6).value = "True"
-                    wb.save(self.Excel_Name)
-                    self.lineEdit_audit.setText("")
-                    print("here")
-        except:
-            QMessageBox.about(self, "Message", "Error ")
-
-
-    #
     def browse_audit(self):
         Excel_File = QFileDialog.getOpenFileName(self, 'Open File', 'Select File', '(*.xlsx)')
         Excel_File = Excel_File[0]
@@ -96,18 +49,51 @@ class BackEndClass(QtWidgets.QWidget, Ui_MainWindow):
             self.insert_btn_audit.setEnabled(True)
             self.lineEdit_audit.setReadOnly(False)
         else:
-            QMessageBox.about(self, "Message", "Please select excel file type")
+            QMessageBox.about(self, "Message", "Please select an Excel file type")
             self.insert_btn_audit.setEnabled(False)
             self.lineEdit_audit.setReadOnly(True)
 
+    def insert_audit(self):
+        AssetTag = str(self.lineEdit_audit.text())
+        print("Asset Tag: " + AssetTag)
+        try:
+            wb = openpyxl.load_workbook(self.Excel_Name)
+            sheet = wb.active
+            row_to_update = None
 
+            for i in range(1, sheet.max_row + 1):
+                if str(sheet.cell(row=i, column=2).value) == AssetTag:
+                    # Mark this row as "True" in column 6
+                    sheet.cell(row=i, column=6).value = "True"
+                    row_to_update = i
+                    break
 
+            if row_to_update:
+                # Save the workbook to persist changes
+                wb.save(self.Excel_Name)
+                # Clear the input field
+                self.lineEdit_audit.setText("")
+                print(f"Row {row_to_update} updated with 'True'")
+                # Update the display label with the updated row
+                self.display_row(row_to_update)
+            else:
+                QMessageBox.about(self, "Message", "Asset Tag not found in the Excel sheet")
 
+        except Exception as e:
+            QMessageBox.about(self, "Message", "Error: " + str(e))
 
+    def display_row(self, row_number):
+        try:
+            # Ensure the workbook is reloaded to reflect any recent changes
+            wb = openpyxl.load_workbook(self.Excel_Name, data_only=True)
+            sheet = wb.active
 
+            # Read the specific row and convert it to a string for display
+            row_data = [str(sheet.cell(row=row_number, column=col).value) for col in range(1, sheet.max_column + 1)]
+            self.textEdit_user.setText(" | ".join(row_data))
 
-
-
+        except Exception as e:
+            QMessageBox.about(self, "Message", "Error: " + str(e))
 
 
 if __name__ == "__main__":
@@ -116,6 +102,3 @@ if __name__ == "__main__":
     ui = BackEndClass()
     MainWindow.show()
     sys.exit(app.exec_())
-
-
-
